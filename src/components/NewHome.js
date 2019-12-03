2448d5f1c4a61e82a490f8e916897b741aafe0d6
Joshua Jones <joshuajones@trentu.ca>
2019-12-03 17:59:18 -0500

    created NewHome component
    -will change this to a functional compoennt after completing basic layout and connection to database

---
+++ src/components/NewHome.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class NewHome extends React.Component {
+	render(){
+		return(
+			<div>
+				HI
+			</div>
+		)
+	}
+}
+
+export default NewHome
--- src/components/Routes.js
+++ src/components/Routes.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import {BrowserRouter, Switch, Route} from 'react-router-dom'
 import Home from './Home'
+import NewHome from './NewHome'
 
 class Routes extends React.Component {
 	render(){
 		return(
 			<BrowserRouter>
 				<Switch>
+					<Route path='/newhome' component={NewHome} />
 					<Route path='/' component={Home} />
 				</Switch>
 			</BrowserRouter>
