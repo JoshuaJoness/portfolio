034cdb3db842e1e99e30b05e05d4517fb25ba808
Joshua Jones <joshuajones@trentu.ca>
2019-12-10 11:00:41 -0500

    removed uncessary class names

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -33,7 +33,7 @@ class Projects extends React.Component {
 							<div className='project-card-container' >
 								<Card className='card'>
 									<CardActionArea>
-										<a className='caloriecounter-link' href="http://caloriecounter.tech/" target='_blank'>
+										<a href="http://caloriecounter.tech/" target='_blank'>
 										<CardMedia
 											component="img"
 											alt="Contemplative Reptile"
@@ -43,7 +43,7 @@ class Projects extends React.Component {
 										/></a>
 										<CardContent>
 											<Typography gutterBottom variant="h5" component="h2">
-											<a className='caloriecounter-link' href="http://caloriecounter.tech/" target='_blank'><h1 className='project-title'>Calorie Counter</h1></a>
+											<a href="http://caloriecounter.tech/" target='_blank'><h1 className='project-title'>Calorie Counter</h1></a>
 											</Typography>
 											<Typography variant="body2" color="textSecondary" component="p">
 											<p>This is a simple calorie calculator that I built with React, Node.js, and Express.</p>
@@ -60,7 +60,7 @@ class Projects extends React.Component {
 
 								<Card className='card'>
 									<CardActionArea>
-										<a className='pilottoronto-link' href="http://pilottoronto.ca/" target='_blank'>
+										<a href="http://pilottoronto.ca/" target='_blank'>
 										<CardMedia
 											component="img"
 											alt="Contemplative Reptile"
@@ -70,7 +70,7 @@ class Projects extends React.Component {
 										/></a>
 										<CardContent>
 											<Typography gutterBottom variant="h5" component="h2">
-												<a className='pilottoronto-link' href="http://pilottoronto.ca/" target='_blank'><h1 className='project-title'>Pilot Pickup</h1></a>
+												<a href="http://pilottoronto.ca/" target='_blank'><h1 className='project-title'>Pilot Pickup</h1></a>
 											</Typography>
 											<Typography variant="body2" color="textSecondary" component="p">
 												<p>This is a project that I built for a client with a mobile first design in mind.</p>
@@ -87,7 +87,7 @@ class Projects extends React.Component {
 
 								<Card className='card'>
 									<CardActionArea>
-										<a className='pilottoronto-link' href="https://airbnb-joshua.herokuapp.com/" target='_blank'>
+										<a href="https://airbnb-joshua.herokuapp.com/" target='_blank'>
 										<CardMedia
 											component="img"
 											alt="Contemplative Reptile"
@@ -97,7 +97,7 @@ class Projects extends React.Component {
 										/></a>
 										<CardContent>
 											<Typography gutterBottom variant="h5" component="h2">
-												<a className='pilottoronto-link' href="https://airbnb-joshua.herokuapp.com/" target='_blank'><h1 className='project-title'>Airbnb Clone</h1></a>
+												<a href="https://airbnb-joshua.herokuapp.com/" target='_blank'><h1 className='project-title'>Airbnb Clone</h1></a>
 											</Typography>
 											<Typography variant="body2" color="textSecondary" component="p">
 												<p>This is an AirBnb clone that I created during my time at Tortuga Coders. This is a fullstack web application built using React, Node.js, MongoDB, and Express. JSON webtokens, B-Crypt, Axios, Cloudinary and Stripe were also used.</p>
@@ -112,7 +112,7 @@ class Projects extends React.Component {
 									</CardActionArea>
 								</Card>
 
-								
+
 							</div>
 						</div>
 
